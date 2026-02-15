ba49c5e061aec1bdaaef95180975e536f487443f
Alex Stokes <r.alex.stokes@gmail.com>
2026-02-14 17:32:34 -0700

    add git fugitive

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,19 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'tpope/vim-fugitive',
+    keys = {
+      { '<leader>gs', '<cmd>Git<CR>', desc = '[G]it [S]tatus' },
+      { '<leader>gd', '<cmd>Gvdiffsplit<CR>', desc = '[G]it [D]iff split' },
+      { '<leader>gb', '<cmd>Git blame<CR>', desc = '[G]it [B]lame' },
+      { '<leader>gl', '<cmd>Git log --oneline --decorate --graph<CR>', desc = '[G]it [L]og' },
+      { '<leader>gp', '<cmd>Git push<CR>', desc = '[G]it [P]ush' },
+      { '<leader>gP', '<cmd>Git pull<CR>', desc = '[G]it [P]ull' },
+      { '<leader>gw', '<cmd>Gwrite<CR>', desc = '[G]it [W]rite (stage file)' },
+      { '<leader>gr', '<cmd>Gread<CR>', desc = '[G]it [R]ead (checkout file)' },
+      { '<leader>gc', '<cmd>Git commit<CR>', desc = '[G]it [C]ommit' },
+    },
+  },
+}
