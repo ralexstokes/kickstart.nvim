84c3cbd63057f2ddaebd893f51e6f0d114dd01ea
Alex Stokes <r.alex.stokes@gmail.com>
2026-02-14 20:48:01 -0700

    Add Neogit plugin to custom module

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,4 +3,25 @@
 --
 -- See the kickstart.nvim README for more information
 return {
+  {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'sindrets/diffview.nvim',
+      'nvim-telescope/telescope.nvim',
+    },
+    cmd = 'Neogit',
+    keys = {
+      {
+        '<leader>gg',
+        function() require('neogit').open() end,
+        desc = 'Open Neo[g]it',
+      },
+    },
+    opts = {
+      integrations = {
+        diffview = true,
+      },
+    },
+  },
 }
